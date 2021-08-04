d830bec205ef3b7ad21f7c81720ea4c9accda4d0
Tabben <nitharsan.nagularajan@yh.nackademin.se>
2021-08-04 12:54:45 +0200

    Changing design from dropdown to dropdown w search

--- src/App.js
+++ src/App.js
@@ -7,17 +7,21 @@ import StockPage from './pages/StockPage'
 import Modal from './components/Modal'
 import { DataContext } from './context/DataContext'
 import { showModal } from './kit/Functions'
-
+import NavPanel from './components/NavPanel'
 function App() {
 
   const { modalData } = useContext(DataContext)
   
   return (
     <div className="App">
-      <button onClick={showModal}>
+      {/* <button onClick={showModal}>
         Show Modal
-      </button>
+      </button> */}
 
+      <NavPanel />
+      <div>
+        content
+      </div>
       <Switch>
         <Route exact path='/stocks' component={ListPage} />
         <Route exact path='/stocks/:id' component={StockPage} />
