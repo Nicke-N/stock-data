7aa25305791a5a8f7eba7231cf812e9f259d8874
Tabben <nitharsan.nagularajan@yh.nackademin.se>
2021-08-20 13:51:53 +0200

    added add-stock page and list updated after

--- src/pages/AddStock.jsx
+++ src/pages/AddStock.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useHistory } from 'react-router-dom'
+import { addStock, getStocks } from '../kit/api/Stocks'
+import { DataContext } from '../context/DataContext'
+import './CrudStyle.css'
 
-export default function AddStock(props) {
+export default function AddStock() {
+
+    const { currentStock, setStockList } = useContext(DataContext)
+    const history = useHistory()
 
     const industries = [
         'Aerospace',
@@ -23,9 +30,49 @@ export default function AddStock(props) {
         'Transport',
         'World wide web'
     ]
+
+    
+    const stockName = document.getElementById('stockName')
     const industry = document.getElementById('industry')
+    const dividend = document.getElementById('dividend')
+    const risk = document.getElementById('risk')
+    const success = document.getElementById('add-stock-success')
+    const fail = document.getElementById('add-stock-failure')
+    const formName = document.getElementById('form-name')
+    const form = document.getElementById('form')
+
+
+    const submitForm = async (event) =>  {
+        event.preventDefault()
+        console.log('submit')
+        const details = {
+            stockName: event.target[0].value, 
+            industry: event.target[1].value,
+            dividend: event.target[2].value,
+            risk: event.target[3].value
+        }
+
+        addStock(details)
+        .then(res => res.text())
+        .then(data =>{
+            if (data === 'Stock was added!') {
+                success.style.display = 'block'
+            } else {
+                fail.style.display = 'block'
+            }
+        })
+        setTimeout(() => {
+            getStocks()
+            .then(res => res.json())
+            .then(data => setStockList(data))
+        }, 3000);
+        
+    }
 
     if (industry) {
+
+        fail.style.display = 'none'
+        success.style.display = 'none'
         industry.textContent = ''
 
         industries.map(element => {
@@ -35,47 +82,57 @@ export default function AddStock(props) {
 
             industry.appendChild(option)
         })
+       
     }
-    
+
+
+
+   
+
     return (
         <div className='crud-container'>
-        <h5 className='crud-type'>
-            {props.type} stock
-        </h5>
-        <div className='crud-content'>
+            <h2 className='crud-type'>
+                Add stock
+            </h2>
 
-            <form action="">
-                <label htmlFor="name">Name</label>
-                <input type="text" maxLength='50' name='name' id='name' />
-         
-
-           
-                <label htmlFor="industry">Industry</label>
-                <select name="industry" id="industry"></select>
-         
+            <div className='outcome'>
+                <div id='add-stock-success'>
+                    The stock was added successfully!
+                </div>
+                <div id='add-stock-failure'>
+                    An unknown error occured, try again later!
+                </div>
+            </div>
             
-           
-                <label htmlFor="dividend">Dividend</label>
-                <input type="number" maxLength='5'id='dividend' name='dividend' />
-           
-
-           
-                <label htmlFor="risk">Risk level</label>
-                <select name="industry" id="industry">
-                    <option value="high">High</option>
-                    <option value="medium">Medium</option>
-                    <option value="low">Low</option>
-                    <option value="unknown">Unknown</option>
-                </select>
-        
-
-                <label htmlFor="dividend">Dividend</label>
-                <input type="number" maxLength='5' name='dividend' />
-
-                <button> Add stock </button>
+            <form className='crud-content' id='form' onSubmit={submitForm}>
+                
+                <div id='form-name' className='crud-pair'>
+                    <label htmlFor='name'>Name</label>
+                    <input type='text' maxLength='50' name='name' id='stockName' required/>
+                </div>
+                
+                <div id='form-industry' className='crud-pair'>
+                    <label htmlFor='industry' >Industry</label>
+                    <select name='industry'  id='industry' required></select>
+                </div>
+                
+                <div id='form-dividend' className='crud-pair'> 
+                    <label htmlFor='dividend'  >Dividend</label>
+                    <input type='number' maxLength='5' name='dividend' id='dividend'/>
+                </div>
+                
+                <div id='form-risk' className='crud-pair'>
+                <   label htmlFor='risk'  >Risk level</label>
+                    <select name='risk' id='risk'>
+                        <option value='high'>High</option>
+                        <option value='medium'>Medium</option>
+                        <option value='low'>Low</option>
+                        <option value='unknown'>Unknown</option>
+                    </select>
+                </div>
+                
+                <button id='form-btn'> Add stock </button>
             </form>
-             
         </div>
-    </div>
     )
 }
