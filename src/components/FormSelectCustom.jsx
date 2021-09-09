e5978be7d85879193c637246832428f8af7bc79e
Tabben <nitharsan.nagularajan@yh.nackademin.se>
2021-09-09 13:03:43 +0200

    Changes to use title instead of value on input (custom)

--- src/components/FormSelectCustom.jsx
+++ src/components/FormSelectCustom.jsx
@@ -10,50 +10,35 @@ export default function FormSelectCustom() {
         const newCheck = event.target.classList[1]
         const valueContainer = document.getElementById(newCheck)
         valueContainer.checked =true
-        setSelectedValue(valueContainer.value)
+        console.log(valueContainer.title)
     }
     return (
        <div>
-            {/* <details>
-                <summary>Test Dropdown</summary>
-                <ul className='list'>
-                    <li className='li'>Item 1</li>
-                    <li className='li'>Item 2</li>
-                    <li className='li'>Item 3</li>
-                    <li className='li'>Item 4</li>
-                    <li className='li'>Item 5</li>
-                    <li className='li'>Item 1</li>
-                    <li className='li'>Item 2</li>
-                    <li className='li'>Item 3</li>
-                    <li className='li'>Item 4</li>
-                    <li className='li'>Item 5</li>
-                </ul>
-            </details> */}
 
             <details className="custom-select">
                 <summary className="radios">
-                    <input type="radio" name="item" id="default" title="Choose" checked />
-                        <input type="radio" name="item" id="item1" value="Item 1" />
-                            <input type="radio" name="item" id="item2" value="Item 2" />
-                                <input type="radio" name="item" id="item3" value="Item 3" />
-                                    <input type="radio" name="item" id="item4" value="Item 4" />
-                                        <input type="radio" name="item" id="item5" value="Item 5" />
+                    <input  type="radio" name="item" id="default" title="Choose" checked  />
+                        <input  type="radio" name="item" id="item1" title="Item 1" />
+                            <input  type="radio" name="item" id="item2" title="Item 2" />
+                                <input  type="radio" name="item" id="item3" title="Item 3" />
+                                    <input  type="radio" name="item" id="item4" title="Item 4" />
+                                        <input  type="radio" name="item" id="item5" title="Item 5" />
                                         </summary>
                                         <ul className="list">
                                             <li className='li item1' for='item1' onClick={click}>
-                                                <label for="item1">Item 1</label>
+                                                Item 1
                                             </li>
                                             <li className='li item2' onClick={click}>
-                                                <label for="item2" >Item 2</label>
+                                                Item 2
                                             </li>
                                             <li className='li item3' onClick={click}>
-                                                <label for="item3" >Item 3</label>
+                                                Item 3
                                             </li>
                                             <li className='li item4' onClick={click}>
-                                                <label for="item4" >Item 4</label>
+                                                Item 4
                                             </li>
                                             <li className='li item5' onClick={click}>
-                                                <label for="item5">Item 5</label>
+                                                Item 5
                                             </li>
                                         </ul>
             </details>
